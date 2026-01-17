25018f73823e967cc81780f2f811fb53629bfe58
Dylan <nalyd27@outlook.com>
2026-01-17 17:06:03 -0300

    Tratando Dados do Pedido.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express"; // Importa o roteador do Express
 import { productsRouter } from "./products-routes"; // Importa as rotas de produtos
 import { tablesRoutes } from "./tables-routes"; // Importa as rotas de mesas
 import { tablesSessionsRoutes } from "./table-sessions-routes"; // Importa as rotas de sessões de mesas
+import { ordersRoutes } from "./orders-routes";
 
 const routes = Router(); // Cria o roteador principal
 
 routes.use("/products", productsRouter); // Usa as rotas de produtos no caminho /products
 routes.use("/tables", tablesRoutes); // Usa as rotas de mesas no caminho /tables
 routes.use("/tables/sessions", tablesSessionsRoutes); // Usa as rotas de sessões de mesas no caminho /tables/sessions
+routes.use("/orders", ordersRoutes); // Usa as rotas de pedidos no caminho /orders
 
 export { routes }; // Exporta o roteador principal
